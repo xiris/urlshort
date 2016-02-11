e3bbb21e037b96355442c77dc95198f78a056309
Sergei Kukhariev <sergei.kukhariev@carmudi.com>
2016-02-11 21:49:51 +0100

    some skeleton files

---
+++ test/ShorteningTest.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Source code file for ${CLASS_NAME} test.
+ *
+ * @author Serge Kukharev <sergei.kukhariev@carmudi.com>
+ */
+class ShorteningTest extends PHPUnit_Framework_TestCase
+{
+
+}
