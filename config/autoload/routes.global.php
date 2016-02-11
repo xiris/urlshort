c59bcafeab505f49a7695c6995654312ee0f3b89
Fabio Carneiro <fahecs@gmail.com>
2016-02-11 22:17:08 +0100

    remove and fix middleware

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -4,10 +4,9 @@ return [
     'dependencies' => [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
-            App\Middleware\PingAction::class => App\Middleware\PingAction::class,
         ],
         'factories' => [
-            App\Middleware\GetShorteningMiddleware::class => App\Middleware\HomePageFactory::class,
+            App\Middleware\GetShorteningMiddleware::class => \App\Middleware\Factory\GetShorteningMiddlewareFactory::class,
         ],
     ],
 
@@ -16,12 +15,6 @@ return [
             'name' => 'home',
             'path' => '/',
             'middleware' => App\Middleware\GetShorteningMiddleware::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'api.ping',
-            'path' => '/api/ping',
-            'middleware' => App\Middleware\PingAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
